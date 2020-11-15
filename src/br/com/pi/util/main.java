83694caf0a01986fd85b85c2429d2f3f12628243
Watlas <watlasrick@hotmail.com>
2020-11-15 17:59:49 -0300

    Primeiro SEGUNDO

--- src/br/com/pi/util/main.java
+++ src/br/com/pi/util/main.java
@@ -3,12 +3,5 @@ package br.com.pi.util;
 import br.com.pi.model.PessoaPF;
 
 public class main {
-    public static void main(String[] args) {
-        PessoaPF cliente = new PessoaPF();
 
-        cliente.setNome("watlas");
-        cliente.setIdade(20);
-
-        System.out.println(cliente.getIdade() + cliente.getNome());
-    }
 }
