aaf22e60d8529feca91180cf5f32fbdbfe9ee0d2
Anisberto Reis <anisbertoos@gmail.com>
2020-12-01 23:27:53 -0300

    Icone View

--- src/br/com/pi/app/LoginView.java
+++ src/br/com/pi/app/LoginView.java
@@ -38,6 +38,7 @@ public class LoginView extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Login");
 
         jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         jLabel1.setText("Projeto Integrador");
