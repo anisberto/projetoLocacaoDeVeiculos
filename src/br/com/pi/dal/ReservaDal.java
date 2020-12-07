f2a9e05c39cec28af687fa5ec704def730dc0e3e
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-12-07 20:52:20 -0300

    Update ReservaDal.java

--- src/br/com/pi/dal/ReservaDal.java
+++ src/br/com/pi/dal/ReservaDal.java
@@ -10,79 +10,103 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
 public class ReservaDal implements ICRUD_GENERIC {
 
     private Connection conexao;
-    ReservaModel reservaModel = new ReservaModel();
+    ReservaModel reserva = new ReservaModel();
 
     public ReservaDal() throws Exception {
         conexao = Conexao.getInstance().getConnection();
     }
 
-   
-
     @Override
     public void add(Object objeto) throws Exception {
-        reservaModel = (ReservaModel) objeto;
-        String sql = "INSERT INTO reserva (reserva_dataFinal, reserva_dataDareserva,reserva_veiculos_idem"
-                + "reserva_pessoas_idem) VALUES (?,?,?,?)";
-        PreparedStatement ps = conexao.prepareStatement(sql);
-        ps.setObject(1, reservaModel.getReserva_dataExpiracao());
-        ps.setObject(2, reservaModel.getReserva_dataExpiracao());
-        ps.setObject(3, reservaModel.getReserva_veiculo());
-        ps.setObject(4, reservaModel.getReserva_cliente());
-        ps.executeUpdate();
+
+        try {
+            reserva = (ReservaModel) objeto;
+            String sql = "INSERT INTO reserva (reserva_datafinal, reserva_datadareserva, reserva_pessoas_idem) VALUES (?, ?, 2)";
+
+            PreparedStatement ps = conexao.prepareStatement(sql);
+            ps.setDate(1, new java.sql.Date(new Date(reserva.getReserva_dataReserva()).getTime()));
+            ps.setDate(2, new java.sql.Date(new Date(reserva.getReserva_dataExpiracao()).getTime()));
+//ps.setObject(1, reserva.getReserva_dataExpiracao());
+            //ps.setObject(2, reserva.getReserva_dataExpiracao());
+            //ps.setObject(3, reserva.getReserva_veiculo());
+            // ps.setObject(4, reserva.getReserva_cliente());
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            throw new Exception("Erro ao incluir: 'Reserva dal' " + e.getMessage());
+        }
 
     }
 
     @Override
     public void delete(int n) throws Exception {
-        String sql = "DELETE FROM reserva WHERE reserva_idem=?";
-        PreparedStatement ps = conexao.prepareStatement(sql);
-        ps.setObject(1, n);
-        ps.executeUpdate();
+        try {
+            String sql = "DELETE FROM reserva WHERE reserva_idem=?";
+            PreparedStatement ps = conexao.prepareStatement(sql);
+            ps.setObject(1, n);
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            throw new Exception("Erro ao deletar: dal.error ");
+        }
 
     }
 
     @Override
     public void update(Object objeto) throws Exception {
-        String sql = "UPDATE reserva SET reserva_dataFinal=?, reserva_dataDareserva=?,reserva_veiculos_idem=?"
-                + "reserva_pessoas_idem=? WHERE reserva_idem=?";
-        PreparedStatement ps = conexao.prepareStatement(sql);
-        ps.setObject(1, reservaModel.getReserva_dataExpiracao());
-        ps.setObject(2, reservaModel.getReserva_dataExpiracao());
-        ps.setObject(3, reservaModel.getReserva_veiculo());
-        ps.setObject(4, reservaModel.getReserva_cliente());
-        ps.setObject(5, reservaModel.getReserva_idem());
-        ps.executeUpdate();
+
+        try {
+            String sql = "UPDATE reserva SET reserva_datafinal=?, reserva_datadareserva=?"
+                    + "reserva_pessoas_idem=? WHERE reserva_idem=?";
+            PreparedStatement ps = conexao.prepareStatement(sql);
+
+            ps.setObject(1, reserva.getReserva_dataExpiracao());
+            ps.setObject(2, reserva.getReserva_dataReserva());
+            //ps.setObject(3, reserva.getReserva_veiculo());
+
+            ps.setObject(3, reserva.getReserva_idem());
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            throw new Exception("Erro ao atualizar: dal.error ");
+        }
 
     }
 
     @Override
     public Iterator getAll() throws Exception {
-        PessoaDal pessoaDal = new PessoaDal();
-        VeiculoDal veiculoDal = new VeiculoDal();
-        String sql = "SELECT * FROM reserva";
-        List<ReservaModel> list = new ArrayList<>();
+        try {
+            PessoaDal pessoaDal = new PessoaDal();
+            VeiculoDal veiculoDal = new VeiculoDal();
+            String sql = "SELECT * FROM reserva";
+            List<ReservaModel> list = new ArrayList<>();
 
-        Statement st = conexao.createStatement();;
-        ResultSet rs = st.executeQuery(sql);
-        while (rs.next()) {
-            reservaModel = new ReservaModel();
-            reservaModel.setReserva_idem(rs.getInt("reserva_idem"));
-            reservaModel.setReserva_dataReserva(rs.getString("reserva_dataDareserva"));
-            reservaModel.setReserva_dataExpiracao(rs.getString("reserva_dataFinal"));
-//            reservaModel.setReserva_dataReserva(rs.getDate("reserva_dataDareserva"));
-//            reservaModel.setReserva_dataExpiracao(rs.getDate("reserva_dataFinal"));
-            reservaModel.setReserva_cliente((PessoaModel) pessoaDal.getById(rs.getInt("reserva_pessoas_idem")));
-            reservaModel.setReserva_veiculo(veiculoDal.getById(rs.getInt("reserva_veiculos_idem")));
-            list.add(reservaModel);
+            Statement st = conexao.createStatement();;
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                reserva = new ReservaModel();
+                reserva.setReserva_idem(rs.getInt("reserva_idem"));
+                reserva.setReserva_dataReserva(rs.getString("reserva_datadareserva"));
+                reserva.setReserva_dataExpiracao(rs.getString("reserva_datafinal"));
+//            reserva.setReserva_dataReserva(rs.getDate("reserva_dataDareserva"));
+//            reserva.setReserva_dataExpiracao(rs.getDate("reserva_dataFinal"));
+                reserva.setReserva_cliente((PessoaModel) pessoaDal.getById(rs.getInt("reserva_pessoas_idem")));
+                //reserva.setReserva_veiculo(veiculoDal.getById(rs.getInt("reserva_veiculos_idem")));
+                list.add(reserva);
+            }
+            //return list.listIterator();
+            return list.iterator();
+            
+        } catch (Exception e) {
+            throw new Exception("Erro ao Listar: " + e.getMessage());
         }
-        //return list.listIterator();
-        return list.iterator();
     }
 
     @Override
@@ -96,28 +120,28 @@ public class ReservaDal implements ICRUD_GENERIC {
         ResultSet rs = preparedStatement.executeQuery();
 
         if (rs.next()) {
-            reservaModel = new ReservaModel();
-            reservaModel.setReserva_idem(rs.getInt("reserva_idem"));
-            reservaModel.setReserva_dataReserva(rs.getString("reserva_dataDareserva"));
-            reservaModel.setReserva_dataExpiracao(rs.getString("reserva_dataFinal"));
-//            reservaModel.setReserva_dataReserva(rs.getDate("reserva_dataDareserva"));
-//            reservaModel.setReserva_dataExpiracao(rs.getDate("reserva_dataFinal"));
-            reservaModel.setReserva_cliente((PessoaModel) pessoaDal.getById(rs.getInt("reserva_pessoas_idem")));
-            reservaModel.setReserva_veiculo(veiculoDal.getById(rs.getInt("reserva_veiculos_idem")));
+            reserva = new ReservaModel();
+            reserva.setReserva_idem(rs.getInt("reserva_idem"));
+            reserva.setReserva_dataReserva(rs.getString("reserva_dataDareserva"));
+            reserva.setReserva_dataExpiracao(rs.getString("reserva_dataFinal"));
+//            reserva.setReserva_dataReserva(rs.getDate("reserva_dataDareserva"));
+//            reserva.setReserva_dataExpiracao(rs.getDate("reserva_dataFinal"));
+            reserva.setReserva_cliente((PessoaModel) pessoaDal.getById(rs.getInt("reserva_pessoas_idem")));
+            reserva.setReserva_veiculo(veiculoDal.getById(rs.getInt("reserva_veiculos_idem")));
 
         }
-        return reservaModel;
+        return reserva;
     }
 
     @Override
     //public Object getByNome(String nome) throws Exception {
     public ReservaModel getByNome(String nome) throws Exception {
         ReservaModel res = new ReservaModel();
-         String sql = "SELECT * FROM administrador WHERE reserva_datafinal=?";
+        String sql = "SELECT * FROM administrador WHERE reserva_datafinal=?";
         PreparedStatement preparedStatement = conexao.prepareStatement(sql);
         preparedStatement.setString(1, nome);
         ResultSet rs = preparedStatement.executeQuery();
-        if(rs.next()){
+        if (rs.next()) {
             res = new ReservaModel();
             res.setReserva_dataExpiracao(rs.getString("reserva_datafinal"));
         }
